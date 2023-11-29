e64dc6f796cf4339c88abdcc3f0821f7080d6c9b
Martin David <me@martindavid.cz>
2023-11-29 17:33:07 +0100

    Docker support added.

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -9,6 +9,8 @@ using Polly.Extensions.Http;
 using Service;
 using Service.Infrastructure;
 using Service.Interfaces;
+using Service.Repository;
+using System.Configuration;
 using System.Net;
 
 namespace OtePrices
@@ -18,6 +20,10 @@ namespace OtePrices
         static async Task Main(string[] args)
         {
             HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            // dotnet run k1=value1 -k2 value2 --alt3=value2 /alt4=value3 --alt5 value5 /alt6 value6
+            // https://learn.microsoft.com/en-us/dotnet/api/microsoft.extensions.configuration.commandlineconfigurationextensions.addcommandline
+            builder.Configuration.AddCommandLine(args);
 
             builder.Configuration
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -48,6 +54,8 @@ namespace OtePrices
             builder.Services.AddKeyedScoped<ICnbService, CnbServiceCached>(nameof(CnbServiceCached));
             builder.Services.AddScoped<IOteCrService, OteCrService>();
 
+            builder.Services.AddSingleton(typeof(IServiceRepository<>), typeof(ServiceRepository<>));
+
             builder.Services.AddScoped<IOteManager, OteManager>();
 
             using IHost host = builder.Build();
@@ -56,9 +64,15 @@ namespace OtePrices
             logger.LogInformation("Host created.");
 
             var oteManager = host.Services.GetRequiredService<IOteManager>();
+            var configuration = host.Services.GetRequiredService<IConfiguration>();
 
-            var startDate = new DateOnly(2023, 1, 1);
-            var endDate = new DateOnly(2023, 11, 28);
+            DateOnly startDate = configuration.GetValue<DateOnly>("startDate");
+            DateOnly endDate = configuration.GetValue<DateOnly>("endDate");
+            if (endDate == DateOnly.MinValue)
+            {
+                endDate = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddDays(1)); // we can obtain price for tomorrow at max
+            }
+
             var otePrices = await oteManager.GetOtePrices(startDate, endDate);
             await oteManager.SaveOtePrices(otePrices);
 
